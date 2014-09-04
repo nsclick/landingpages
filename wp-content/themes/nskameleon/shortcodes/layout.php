220a371e336bfb18be9bafe9109883f25a7ac24c
CesarReyes <creyes@nsclick.cl>
2014-09-04 10:57:10 -0400

    Plugin updates

--- wp-content/themes/nskameleon/shortcodes/layout.php
+++ wp-content/themes/nskameleon/shortcodes/layout.php
@@ -1,4 +1,5 @@
 <?php
+//http://chevroletinalco.cl/wp-content/uploads/new-cars/485a5665-b81c-3863-d33c-5266eca822b1/aveo_sedan_fb.jpg
 
 //[box_a link="" img_url="" img_title=""]
 function ns_box_a_shortcode( $atts ) {
@@ -59,7 +60,7 @@ function ns_select_contact_shortcode( $atts ) {
 	
 ?> 
 	<ul class="select_contacto">
-		<li><img src="wp-content/themes/nskameleon/images/ico-fono.png" alt="Inalco" title="Inalco" class="ico-fono"/>Contacte su Sucursal INALCO<span class="ico-arrow"><img src="wp-content/themes/nskameleon/images/ico-arrowdown.png" alt="Inalco" title="Inalco"/></span>
+		<li><img src="/wp-content/themes/nskameleon/images/ico-fono.png" alt="Inalco" title="Inalco" class="ico-fono"/>Contacte su Sucursal INALCO<span class="ico-arrow"><img src="/wp-content/themes/nskameleon/images/ico-arrowdown.png" alt="Inalco" title="Inalco"/></span>
 			<ul>
 				<?php foreach($branches as $line): ?>
 					<li><?php echo $line ?></li>
@@ -428,17 +429,17 @@ register_vc_shortcode(array(
 )
 ));
 
-function ns_selector_shortcode( $atts ) {
+function ns_models_selector_shortcode( $atts ) {
 ob_start();
 ?>
 <div class="selector">
 	<div class="auto">
-		<div class="foto"><img src="/landingpages/wp-content/themes/nskameleon/images/cruze.jpg" alt="Modelo y versi&oacute;n" title="Modelo y versi&oacute;n" /></div>
+		<div class="foto"><img src="/wp-content/themes/nskameleon/images/cruze.jpg" alt="Modelo y versi&oacute;n" title="Modelo y versi&oacute;n" /></div>
 		<div class="texto"><b>Chevrolet Cruze</b>Sedán 1.8 MT LS<span>Desde <b>$8.590.000</b></span></div>
 		<div class="switcher">
 			<div class="onoffswitch">
 				<input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" >
-				<label class="onoffswitch-label" for="myonoffswitch"> <span class="onoffswitch-inner"> <span class="onoffswitch-active"><span class="onoffswitch-switch"><img src="/landingpages/wp-content/themes/nskameleon/images/ico-yess.png" alt="Inalco" title="Inalco" style="margin:8px 0 0 0" /></span></span> <span class="onoffswitch-inactive"><span class="onoffswitch-switch"><img src="/landingpages/wp-content/themes/nskameleon/images/ico-arrowright.png" alt="Inalco" title="Inalco" style="margin:8px 0 0 0" /></span></span> </span> </label>
+				<label class="onoffswitch-label" for="myonoffswitch"> <span class="onoffswitch-inner"> <span class="onoffswitch-active"><span class="onoffswitch-switch"><img src="/wp-content/themes/nskameleon/images/ico-yess.png" alt="Inalco" title="Inalco" style="margin:8px 0 0 0" /></span></span> <span class="onoffswitch-inactive"><span class="onoffswitch-switch"><img src="/wp-content/themes/nskameleon/images/ico-arrowright.png" alt="Inalco" title="Inalco" style="margin:8px 0 0 0" /></span></span> </span> </label>
 			</div>
 		</div>
 	</div>
@@ -516,16 +517,53 @@ ob_start();
 <?php
 return ob_get_clean();
 }
-add_shortcode( 'selector', 'ns_selector_shortcode' );
+add_shortcode( 'models_selector', 'ns_models_selector_shortcode' );
 
 register_vc_shortcode(array(
 
-	"name" => __("NSK Form Servicios", 'nskameleon'),
-	"base" => "form_servicios",
+	"name" => __("NSK Modelos", 'nskameleon'),
+	"base" => "models_selector",
 	"class" => "",
 	"category" => __('Content', 'nskameleon'),
-	//'admin_enqueue_js' => array(get_template_directory_uri() . '/vc_extend/bartag.js'),
-	//'admin_enqueue_css' => array(get_template_directory_uri() . '/vc_extend/bartag.css'),
 	"params" => array(
-				)
+		array(
+			"type" => "checkbox",
+			"holder" => "div",
+			"class" => "",
+			"heading" => __('Mostrar el precio?', 'nskameleon'),
+			"param_name" => "price",
+			"value" => array('Si' => 'si'),
+			//"description" => __("Una sucursal por linea.", 'nskameleon')
+		),
+		array(
+			"type" => "checkbox",
+			"holder" => "div",
+			"class" => "",
+			"heading" => __('Mostrar el la foto?', 'nskameleon'),
+			"param_name" => "photo",
+			"value" => array('Si' => 'si'),
+			//"description" => __("Una sucursal por linea.", 'nskameleon')
+		),
+		array(
+			"type" => "ns_models",
+			"holder" => "div",
+			"class" => "",
+			"heading" => __('Modelos', 'nskameleon'),
+			"param_name" => "models",
+			//"value" => array('Si' => 'si'),
+			//"description" => __("Una sucursal por linea.", 'nskameleon')
+		)
+	)
 ));
+
+function ns_param_models_field($settings, $value) {
+	// echo '<pre>',print_r($settings),'</pre>';
+	$dependency = vc_generate_dependencies_attributes($settings);
+	return '<div class="my_param_block">'
+             .'<input name="'.$settings['param_name']
+             .'" class="wpb_vc_param_value wpb-textinput '
+             .$settings['param_name'].' '.$settings['type'].'_field" type="text" value="'
+             .$value.'" ' . $dependency . '/>'
+         .'</div>';
+}
+add_shortcode_param('ns_models', 'ns_param_models_field');
